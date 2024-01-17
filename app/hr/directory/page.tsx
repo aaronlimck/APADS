6160088009ab9cb183f306a4f683e6d749c61b96
Cytan2000 <tanchiyong00@gmail.com>
2024-01-17 15:07:00 +0800

    Update ui

--- app/hr/directory/page.tsx
+++ app/hr/directory/page.tsx
@@ -7,15 +7,22 @@ export default function DirectoryPage() {
     <div className="min-h-screen max-w-7xl mx-auto px-4 md:px-6 py-4">
       <div className="flex justify-between">
         <div className="mb-4">
-          <h1 className="text-3xl font-semibold tracking-tight select-none">Directory</h1>
+          <h1 className="text-3xl font-semibold tracking-tight select-none">
+            Directory
+          </h1>
         </div>
-        <div className="">
-          <Button>+ Add new Employee</Button>
+        <div>
+          <Button className="text-center sm:text-base">
+            <span className="text-xl sm:text-base">+</span>
+            <span className="hidden sm:block ">&nbsp;Add new Employee</span>
+          </Button>
         </div>
       </div>
       <div className="flex">
         <InputSearch></InputSearch>
-        <Button className="border bg-white text-black hover:bg-gray-50 ml-4" >Filter</Button>
+        <Button className="border bg-white text-black hover:bg-gray-50 ml-4">
+          Filter
+        </Button>
       </div>
       <div className="my-4">
         <StaffTable></StaffTable>
