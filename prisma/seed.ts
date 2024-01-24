8e47b95ed0f92dae7762a2662d5fa81c0a13764f
aaron <aaronlimchengkiat@gmail.com>
2024-01-25 03:07:55 +0800

    update seeding script

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,73 +1,65 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient, Role } from "@prisma/client";
 const prisma = new PrismaClient();
 
+async function createDepartment(name: string) {
+  return prisma.department.upsert({
+    where: { name },
+    update: {},
+    create: { name }
+  });
+}
+
+async function createUser(
+  email: string,
+  name: string,
+  password: string,
+  role: Role,
+  departmentName: string
+) {
+  return prisma.$transaction(async (prisma) => {
+    const user = await prisma.user.upsert({
+      where: { email },
+      update: {},
+      create: { email, name, password, role }
+    });
+
+    // Create a Staff record associated with the newly created user
+    await prisma.staff.upsert({
+      where: { userId: user.id },
+      update: {},
+      create: { userId: user.id, departmentName: departmentName }
+    });
+
+    return user;
+  });
+}
 async function main() {
-  // Create departments
-  const hrDepartment = await prisma.department.upsert({
-    where: { name: "Human Resource" },
-    update: {},
-    create: {
-      name: "Human Resource"
-    }
-  });
+  const hrDepartment = await createDepartment("Human Resource");
+  const financeDepartment = await createDepartment("Finance");
+  const ITDepartment = await createDepartment("Information Technology");
+  const marketingDepartment = await createDepartment("Marketing");
 
-  const financeDepartment = await prisma.department.upsert({
-    where: { name: "Finance" },
-    update: {},
-    create: {
-      name: "Finance"
-    }
-  });
-
-  const ITDepartment = await prisma.department.upsert({
-    where: { name: "Infomation Technology" },
-    update: {},
-    create: {
-      name: "Infomation Technology"
-    }
-  });
-
-  const marketingDepartment = await prisma.department.upsert({
-    where: { name: "Marketing" },
-    update: {},
-    create: {
-      name: "Marketing"
-    }
-  });
-
-  // Create users
-  const HR = await prisma.user.upsert({
-    where: { email: "manager@apads.com" },
-    update: {},
-    create: {
-      email: "manager@apads.com",
-      name: "Manager",
-      password: "Password@888",
-      role: "MANAGER"
-    }
-  });
-
-  const manager = await prisma.user.upsert({
-    where: { email: "hr@apads.com" },
-    update: {},
-    create: {
-      email: "hr@apads.com",
-      name: "HR",
-      password: "Password@888",
-      role: "ADMIN"
-    }
-  });
-
-  const staff = await prisma.user.upsert({
-    where: { email: "staff@apads.com" },
-    update: {},
-    create: {
-      email: "staff@apads.com",
-      name: "Staff",
-      password: "Password@888",
-      role: "STAFF"
-    }
-  });
+  const HR = await createUser(
+    "hr@apads.com",
+    "HR",
+    "Password@888",
+    "MANAGER",
+    "Human Resource"
+  );
+  const manager = await createUser(
+    "manager@apads.com",
+    "Manager",
+    "Password@888",
+    "ADMIN",
+    "Finance"
+  );
+  const staff = await createUser(
+    "staff@apads.com",
+    "Staff",
+    "Password@888",
+    "STAFF",
+    "Finance"
+  );
 }
 
 main()
