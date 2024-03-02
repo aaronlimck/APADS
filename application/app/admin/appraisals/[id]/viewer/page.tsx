fa0b883af302b7d60adc5a35b516ecc61ff9d455
Herman Tan <hermanblueberry@gmail.com>
2024-03-02 16:51:55 +0800

    Update page.tsx

--- application/app/admin/appraisals/[id]/viewer/page.tsx
+++ application/app/admin/appraisals/[id]/viewer/page.tsx
@@ -4,8 +4,8 @@ import {
 import { notFound } from "next/navigation";
 import {
   FormElementInstance,
-  FormElements,
 } from "@/components/form-builder/form-elements";
+import ViewerComponent from "@/components/viewer/viewer-component";
 
 export default async function AdminAppraisalsBuilderPage({
   params,
@@ -20,26 +20,6 @@ export default async function AdminAppraisalsBuilderPage({
     const formContent = JSON.parse(form.data.content) as FormElementInstance[];
 
     return (
-      <>
-        <div className="border-b p-4">
-          <p className="text-muted-foreground">Form Preview</p>
-        </div>
-        <div className="flex flex-grow flex-col items-center justify-center overflow-y-auto bg-accent p-4 pt-[76px]">
-          <div className="flex min-h-dvh w-full max-w-3xl flex-grow flex-col gap-4 space-y-4 bg-white px-4 py-8">
-            {formContent.map((element) => {
-              console.log(element.type)
-              const FormElement = FormElements[element.type].formComponent;
-              return (
-                <>
-                  <FormElement
-                    key={element.id}
-                    elementInstance={element}
-                  />
-                </>
-              );
-            })}
-          </div>
-        </div>
-      </>
-    );
+      <ViewerComponent content={formContent} />
+      );
 }
